9222856e69b19e598fcb590c640065f7516e3d86
ashloverscn <ashloverscn@gmail.com>
2023-11-25 04:26:24 +0530

    Add files via upload
    Python using Rotating Proxy vs Direct Connection

---
+++ proxy_request.py
@@ -0,0 +1,22 @@
+import time
+import requests
+
+proxies = {
+    #'http': 'socks5://ash:ash@192.168.85.221:1080',
+    #'https': 'socks5://ash:ash@192.168.85.221:1080'
+    'http': 'socks5://aTtK3zBio1zplQyB:xJo9aGmyF6m4nGbb_streaming-1@103.214.44.131:32325',
+    'https': 'socks5://aTtK3zBio1zplQyB:xJo9aGmyF6m4nGbb_streaming-1@103.214.44.131:32325'
+}
+
+#url = 'https://ifconfig.me'
+url = 'http://ip.42.pl/raw'
+
+#for x in range(6):
+while True:
+    r = requests.get(url)
+    print(f'Status Code Direct: {r.status_code}')
+    print(f'Raw Respones Direct: {r.text}')
+    r = requests.get(url,  proxies=proxies)
+    print(f'Status Code Proxied: {r.status_code}')
+    print(f'Raw Respones Proxied: {r.text}')
+    time.sleep(1)
