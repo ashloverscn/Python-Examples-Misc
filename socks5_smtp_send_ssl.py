d1192db38b2b20bd7967c8ded831b4929a269fb1
ashloverscn <ashloverscn@gmail.com>
2023-11-27 04:23:37 +0530

    Add files via upload

---
+++ socks5_smtp_send_ssl.py
@@ -0,0 +1,31 @@
+import ssl
+import smtplib
+import socks
+
+#socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, addr=socks_active['ip'], port=socks_active['port'],username=socks_active['username'],password=socks_active['password'])
+
+# Fails to connect to Gmail through Rotating Proxy with error: socks.SOCKS5Error: 0x02: Connection not allowed by ruleset #
+#socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, addr='geo.iproyal.com' , port=32325 , rdns=True, username='aTtK3zBio1zplQyB' ,password='xJo9aGmyF6m4nGbb_streaming-1' )
+
+# Local proxy with Every Proxy on Google Play Store #
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, addr='192.168.85.221' , port=1080 ,rdns=True, username='ash' ,password='ash')  
+
+socks.wrapmodule(smtplib)
+
+smtpserver = 'smtp.gmail.com'
+AUTHREQUIRED = 1 
+smtpuser = 'ashloverscn@gmail.com'  
+smtppass = 'gckb lspa bjzg xrna'  
+
+RECIPIENTS = 'ash.temp.new@gmail.com'
+SENDER = 'ashloverscn@gmail.com'
+mssg = "test message"
+s = mssg   
+
+context = ssl.create_default_context()
+
+server = smtplib.SMTP_SSL(smtpserver, 465, context=context )
+server.login(smtpuser,smtppass)
+server.set_debuglevel(1)
+server.sendmail(SENDER, [RECIPIENTS], s)
+server.quit()
