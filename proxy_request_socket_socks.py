010423f215445630f9fd1ad32efc02ff65f1fabb
ashloverscn <ashloverscn@gmail.com>
2023-11-27 04:49:04 +0530

    Update proxy_request_socket_socks.py

--- proxy_request_socket_socks.py
+++ proxy_request_socket_socks.py
@@ -3,7 +3,8 @@ import socket
 import socks
 import requests
 
-socks.setdefaultproxy(socks.SOCKS5, 'geo.iproyal.com', 32325, True, 'aTtK3zBio1zplQyB', 'xJo9aGmyF6m4nGbb_streaming-1')
+#socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'geo.iproyal.com', 32325, True, 'aTtK3zBio1zplQyB', 'xJo9aGmyF6m4nGbb_streaming-1')
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, addr='192.168.85.221' , port=1080 ,rdns=True, username='ash' ,password='ash')
 socket.socket = socks.socksocket
 #url = 'https://ifconfig.me'
 url = 'http://ip.42.pl/raw'
