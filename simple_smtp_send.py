bc6ec5189f2e4c1389667fba1c1202bd8aa4ad5b
ashloverscn <ashloverscn@gmail.com>
2023-11-26 19:02:58 +0530

    Add files via upload
    smtplib example in few lines

---
+++ simple_smtp_send.py
@@ -0,0 +1,20 @@
+import smtplib
+
+smtpserver = 'smtp.gmail.com'
+AUTHREQUIRED = 1 
+smtpuser = 'ashloverscn@gmail.com'  
+smtppass = 'gckb lspa jitx xrna'  
+
+RECIPIENTS = 'ash.temp.new@gmail.com'
+SENDER = 'ashloverscn@gmail.com'
+mssg = "test message"
+s = mssg   
+
+server = smtplib.SMTP(smtpserver,587)
+server.ehlo()
+server.starttls() 
+server.ehlo()
+server.login(smtpuser,smtppass)
+server.set_debuglevel(1)
+server.sendmail(SENDER, [RECIPIENTS], s)
+server.quit()
