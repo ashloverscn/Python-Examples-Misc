181980510ea2a53807d16c43992d5e337170c5bf
ashloverscn <ashloverscn@gmail.com>
2023-11-26 18:44:20 +0530

    Add files via upload
    proxy request using socket socks

---
+++ proxy_request_socket_socks.py
@@ -0,0 +1,16 @@
+import time
+import socket
+import socks
+import requests
+
+socks.setdefaultproxy(socks.SOCKS5, '103.214.44.131', 32325, True, 'aTtK3zBio1zplQyB', 'xJo9aGmyF6m4nGbb_streaming-1')
+socket.socket = socks.socksocket
+#url = 'https://ifconfig.me'
+url = 'http://ip.42.pl/raw'
+
+#for x in range(6):
+while True:
+    r = requests.get(url)    
+    print(f'Status Code Proxied: {r.status_code}')
+    print(f'Raw Respones Proxied: {r.text}')
+    time.sleep(1)
