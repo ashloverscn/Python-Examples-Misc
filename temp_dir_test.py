8abd7cdc955a34382cde08603c9ba207d8fe1cad
ashloverscn <ashloverscn@gmail.com>
2024-03-30 01:12:12 +0530

    Add files via upload

--- temp_dir_test.py
+++ temp_dir_test.py
@@ -1,4 +1,5 @@
 #https://stackoverflow.com/questions/847850/cross-platform-way-of-getting-temp-directory-in-python
+#https://java2blog.com/get-temp-directory-python/
 from pathlib import Path
 import platform
 import tempfile
@@ -6,3 +7,16 @@ import tempfile
 tempdir = Path("/tmp" if platform.system() == "Darwin" else tempfile.gettempdir())
 
 print(tempdir)
+
+import tempfile
+path = tempfile.gettempdir()
+print(path)
+
+import tempfile
+path = tempfile.gettempdirb()
+print(path)
+
+import tempfile
+path = tempfile.tempdir
+print(path)
+
