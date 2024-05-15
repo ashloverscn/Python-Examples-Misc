abd121a6435d4a6f7ccd49148e672a22fe1e558c
ashloverscn <ashloverscn@gmail.com>
2024-05-15 19:05:29 +0530

    Add files via upload

---
+++ compil_test__gui.bat
@@ -0,0 +1,32 @@
+@ echo off
+pip install pyinstaller
+pyinstaller --clean --noconsole --onefile --icon=Gmail-API-X.ico --add-data "Gmail-API-X.ico;." test__gui.py
+move /y .\dist\*.exe .\
+del /f /q /a .\*.spec
+rmdir /s /q .\build .\dist
+
+rem #### This are also valid commands ####
+rem #for non-gui without any disp icon# pyinstaller --clean --onefile script_name.py
+rem #for non-gui with only disp icon# pyinstaller --clean --onefile --icon=icon_file_name.ico script_name.py
+rem #for non-gui with disp and MIMEPASS icon#
+rem pyinstaller --clean --onefile --icon=icon_file_name.ico --add-data "icon_file_name.ico;." script_name.py
+rem #for gui with disp and MIMEPASS icon#
+rem pyinstaller --clean --noconsole --onefile --icon=icon_file_name.ico --add-data "icon_file_name.ico;." script_name.py
+
+rem move /y .\dist\*.exe .\
+rem move /y .\dist\*.exe
+rem move /y dist\*.exe .\
+rem move /y dist\*.exe
+
+rem copy /b /y .\dist\*.exe .\
+rem copy /b /y .\dist\*.exe
+rem copy /b /y dist\*.exe .\
+rem copy /b /y dist\*.exe
+
+rem del /f /q /a .\*.spec
+rem del /f /q /a *.spec
+
+rem rmdir /s /q .\build\ .\dist\
+rem rmdir /s /q .\build\ .\dist\
+rem rmdir /s /q build\ dist\
+rem rmdir /s /q build dist
