173022a3a11eeef9f8c4268bde4c9e5e03926cb5
ashloverscn <ashloverscn@gmail.com>
2024-07-22 23:48:14 +0530

    Add files via upload

--- Split-O-Splice.py
+++ Split-O-Splice.py
@@ -2,53 +2,9 @@ import os
 import pandas as pd
 import re
 
-config_data_file = 'Split-O-Splice.config'
-
-def read_config_file(filename):
-    config = {}
-    with open(filename, 'r') as f:
-        for line in f:
-            # Ignore comments and empty lines
-            if line.strip() and not line.strip().startswith('#'):
-                key, value = line.strip().split('=')
-                config[key.strip()] = value.strip()
-    return config
-
-# Reading variables from the config dictionary
-config = read_config_file(config_data_file)
-max_directory = config.get('max_directory') # data-division or interger-number
-chunk_size = int(config.get('chunk_size')) # interger-number
-chunk_stamp = config.get('chunk_stamp') # colour-yellow or delete-rows-on-finish
-test_interval = int(config.get('test_interval')) # interger-number
-
 # Read data from Excel file
 data_xlsx_file = 'data.xlsx'
 df = pd.read_excel(data_xlsx_file)
-
-# Count the number of valid data rows (assuming any non-null row is valid)
-data_rows_valid = df.dropna().shape[0]
-# Calculate max_directory based on valid data rows
-if max_directory  == "data-division":
-    if data_rows_valid > 0:
-        max_directory = data_rows_valid // chunk_size
-        if data_rows_valid % chunk_size != 0:
-            max_directory = max_directory +1
-        
-# Printing out the variables
-print("max_directory:", max_directory)
-print("chunk_size:", chunk_size)
-print("chunk_stamp:", chunk_stamp)
-print("test_interval:", test_interval)
-
-def create_directories(n):
-    # Loop from 1 to n (inclusive)
-    for i in range(1, n + 1):
-        # Create directory with name as the number
-        directory_name = str(i)
-        os.makedirs(directory_name, exist_ok=True)
-        print(f"Directory '{directory_name}' created.")
-
-create_directories(max_directory)
 
 # Shift the rows down by one time
 df = df.shift(periods=1)
@@ -103,3 +59,51 @@ df.insert(df.columns.get_loc('repeat') + 1, 'sent', '')
 # Write formated data back to XLSX file
 df.to_excel(data_xlsx_file, index=False)
 print(f'Data successfully formated and written back to {data_xlsx_file}.')
+
+config_data_file = 'Split-O-Splice.config'
+
+def read_config_file(filename):
+    config = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            # Ignore comments and empty lines
+            if line.strip() and not line.strip().startswith('#'):
+                key, value = line.strip().split('=')
+                config[key.strip()] = value.strip()
+    return config
+
+# Reading variables from the config dictionary
+config = read_config_file(config_data_file)
+max_directory = config.get('max_directory') # data-division or interger-number
+chunk_size = int(config.get('chunk_size')) # interger-number
+chunk_stamp = config.get('chunk_stamp') # colour-yellow or delete-rows-on-finish
+test_interval = int(config.get('test_interval')) # interger-number
+
+# Count the number of valid data rows (assuming any non-null row is valid)
+data_rows_valid = df.dropna().shape[0]
+# Calculate max_directory based on valid data rows
+if max_directory  == "data-division":
+    if data_rows_valid > 0:
+        max_directory = data_rows_valid // chunk_size
+        if data_rows_valid % chunk_size != 0:
+            max_directory = max_directory +1
+        
+# Printing out the variables
+print("valid_data_rows:", data_rows_valid)
+print("max_directory:", max_directory)
+print("chunk_size:", chunk_size)
+print("chunk_stamp:", chunk_stamp)
+print("test_interval:", test_interval)
+
+def create_directories(n):
+    # Loop from 1 to n (inclusive)
+    for i in range(1, n + 1):
+        # Create directory with name as the number
+        directory_name = str(i)
+        os.makedirs(directory_name, exist_ok=True)
+        print(f"Directory '{directory_name}' created.")
+
+create_directories(max_directory)
+
+input("Press any Key to Exit")
+
