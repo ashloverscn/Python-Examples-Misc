3a5b3b23dbe4f38a4894742fca1ef563d0a38c36
ashloverscn <ashloverscn@gmail.com>
2024-07-26 14:47:25 +0530

    Add files via upload

--- copy Specified columns names-or-numbers  from csv-or-xlsx to csv-and-xlsx.py
+++ copy Specified columns names-or-numbers  from csv-or-xlsx to csv-and-xlsx.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Define the file paths
-input_file_csv = 'contacts.csv'
-input_file_xlsx = 'contacts.xlsx'
-output_file_csv = 'contacts1.csv'
-output_file_xlsx = 'contacts1.xlsx'
+input_file_csv = 'ContactsData.csv'
+input_file_xlsx = 'ContactsData.xlsx'
+output_file_csv = 'contacts.csv'
+output_file_xlsx = 'contacts.xlsx'
 
 # Read the first 250 rows from specific columns
 #Read csv input file using row header name
@@ -19,6 +19,6 @@ df = pd.read_excel(input_file_xlsx, usecols=['name', 'email', 'repeat', 'sent'],
 df = pd.read_excel(input_file_xlsx, usecols=[0, 1, 2, 3], nrows=250)
 
 # Write to contacts1.csv
-df.to_csv(output_file_csv, index=False)
+df.to_csv(output_file_csv, index=False, na_rep='')
 # Write to contacts1.xlsx
-df.to_excel(output_file_xlsx, index=False)
+df.to_excel(output_file_xlsx, index=False, na_rep='')
