c26eca2b05d9fa322fb8c502c944194add1691c4
ashloverscn <ashloverscn@gmail.com>
2025-07-17 22:35:52 +0530

    Create password-gen.py

---
+++ password-gen.py
@@ -0,0 +1,32 @@
+import random
+
+# Full set of printable US keyboard characters (excluding whitespace)
+characters = (
+    'abcdefghijklmnopqrstuvwxyz'
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    '0123456789'
+    '~!@#$%^&*()_+`-={}|[]\\:";\'<>?,./'
+)
+
+# Ask user for number of characters
+user_input = input("Enter the number of characters to scramble (default is 8): ").strip()
+
+# Default to 8 if no valid number is provided
+try:
+    num_chars = int(user_input)
+    if num_chars <= 0:
+        raise ValueError
+except ValueError:
+    num_chars = 8
+
+# Randomly select without replacement if possible
+if num_chars <= len(characters):
+    scrambled = ''.join(random.sample(characters, num_chars))
+else:
+    scrambled = ''.join(random.choices(characters, k=num_chars))  # allow duplicates if needed
+
+# Append to file
+with open('scrambled_output.txt', 'a') as file:
+    file.write(scrambled + '\n')
+
+print(f"Scrambled string ({num_chars} chars) written to scrambled_output.txt")
